68078685c66d357273d89f163e53ba75efd25bc4
abysmal123 <1611316689@qq.com>
2023-04-13 19:03:30 +0800

    Update injectivityCircleCount.java

--- src/main/java/ca/injectivitycircle/injectivityCircleCount.java
+++ src/main/java/ca/injectivitycircle/injectivityCircleCount.java
@@ -5,54 +5,8 @@ import ca.catools.Tools;
 
 public final class injectivityCircleCount {         // d <= 5
 
-    // 统计环，移位相等的配置视为相同配置，无需考虑中心位置（左右半径）
-    public static void showCircleList(String r, int d, int n) {
-        int rule = Tools.getRule(r, 1 << d);
-        int ruleMask = (1 << d) - 1;
-        int lr = (d - 1) >> 1;
-        int rr = d >> 1;
-        int lBoundaryMask = (1 << lr) - 1;
-        int rBoundaryMask = (1 << rr) - 1;
-        int maxConfig = (1 << n) - 1;
-        Map<Integer, Integer> loopCountMap = new TreeMap<>();
-        boolean[] visited = new boolean[1 << n];
-        for (int i = 0; i <= maxConfig; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            int loop = 0;
-            int curConfig = i;
-            while (!visited[curConfig]) {
-                int periodicConfig = curConfig;
-                for (int k = 0; k < n; k++) {
-                    if (visited[periodicConfig]) {
-                        break;
-                    }
-                    visited[periodicConfig] = true;
-                    periodicConfig = ((periodicConfig << 1) & maxConfig) | ((periodicConfig >> (n - 1)) & 1);
-                }
-                int preimage = ((curConfig & lBoundaryMask) << (n + rr)) | (curConfig << rr) | ((curConfig >> (n - rr)) & rBoundaryMask);
-                int image = 0;
-                for (int j = 0; j < n; j++) {
-                    image <<= 1;
-                    image |= (rule >> ((preimage >> (n - 1 - j)) & ruleMask)) & 1;
-                }
-                curConfig = image;
-                loop++;
-            }
-            loopCountMap.put(loop, loopCountMap.getOrDefault(loop, 0) + 1);
-        }
-        System.out.println("rule:\t\t" + r);
-        System.out.println("n:   \t\t" + n);
-        System.out.println("length\tcount");
-        System.out.println("--------------");
-        for (Map.Entry<Integer, Integer> entry : loopCountMap.entrySet()) {
-            System.out.println(entry.getKey() + "\t\t" + entry.getValue());
-        }
-    }
-
-    // 统计环，移位相等的配置视为不同，需设置中心位置（左右半径）
-    public static void showCircleList(String r, int lr, int rr, int n) {
+    // 统计环（规则，左半径，右半径，细胞数，移位相同是否视为同一配置）
+    public static void showCircleList(String r, int lr, int rr, int n, boolean shiftingAsOne) {
         int d = lr + rr + 1;
         int rule = Tools.getRule(r, 1 << d);
         int ruleMask = (1 << d) - 1;
@@ -68,91 +22,40 @@ public final class injectivityCircleCount {         // d <= 5
             int loop = 0;
             int curConfig = i;
             while (!visited[curConfig]) {
-                visited[curConfig] = true;
-                int preimage = ((curConfig & lBoundaryMask) << (n + rr)) | (curConfig << rr) | ((curConfig >> (n - rr)) & rBoundaryMask);
-                int image = 0;
-                for (int j = 0; j < n; j++) {
-                    image <<= 1;
-                    image |= (rule >> ((preimage >> (n - 1 - j)) & ruleMask)) & 1;
-                }
-                curConfig = image;
-                loop++;
-            }
-            loopCountMap.put(loop, loopCountMap.getOrDefault(loop, 0) + 1);
-        }
-        System.out.println("rule:\t\t" + r);
-        System.out.println("n:   \t\t" + n);
-        System.out.println("length\tcount");
-        System.out.println("--------------");
-        for (Map.Entry<Integer, Integer> entry : loopCountMap.entrySet()) {
-            System.out.println(entry.getKey() + "\t\t" + entry.getValue());
-        }
-    }
-
-    // 打印特定长度的环，移位相等的配置视为相同配置
-    public static void showCircleWithLength(String r, int d, int n, int lengthForPrint, int minNum, int maxNum) {
-        int rule = Tools.getRule(r, 1 << d);
-        int ruleMask = (1 << d) - 1;
-        int lr = (d - 1) >> 1;
-        int rr = d >> 1;
-        int lBoundaryMask = (1 << lr) - 1;
-        int rBoundaryMask = (1 << rr) - 1;
-        int maxConfig = (1 << n) - 1;
-        Map<Integer, Integer> loopCountMap = new TreeMap<>();
-        boolean[] visited = new boolean[1 << n];
-        int circleCnt = 0;
-        String arrow = getArrow(n);
-        System.out.println("rule:\t\t\t" + r);
-        System.out.println("n:   \t\t\t" + n);
-        System.out.println("circle length:\t" + lengthForPrint);
-        System.out.println("-------------------------------");
-        for (int i = 0; i <= maxConfig; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            List<Integer> evolution = new ArrayList<>();
-            int loop = 0;
-            int curConfig = i;
-            while (!visited[curConfig]) {
-                evolution.add(curConfig);
-                int periodicConfig = curConfig;
-                for (int k = 0; k < n; k++) {
-                    if (visited[periodicConfig]) {
-                        break;
-                    }
-                    visited[periodicConfig] = true;
-                    periodicConfig = ((periodicConfig << 1) & maxConfig) | ((periodicConfig >> (n - 1)) & 1);
-                }
-                int preimage = ((curConfig & lBoundaryMask) << (n + rr)) | (curConfig << rr) | ((curConfig >> (n - rr)) & rBoundaryMask);
-                int image = 0;
-                for (int j = 0; j < n; j++) {
-                    image <<= 1;
-                    image |= (rule >> ((preimage >> (n - 1 - j)) & ruleMask)) & 1;
-                }
-                curConfig = image;
-                loop++;
-            }
-            evolution.add(curConfig);
-            if (loop == lengthForPrint) {
-                circleCnt++;
-                if (circleCnt >= minNum && circleCnt <= maxNum) {
-                    System.out.println("Circle No." + circleCnt + ":");
-                    for (int idx = 0; idx <= lengthForPrint; idx++) {
-                        System.out.println(Tools.toNBitString(evolution.get(idx), n));
-                        if (idx < lengthForPrint) {
-                            System.out.println(arrow);
+                if (shiftingAsOne) {
+                    int periodicConfig = curConfig;
+                    for (int k = 0; k < n; k++) {
+                        if (visited[periodicConfig]) {
+                            break;
                         }
+                        visited[periodicConfig] = true;
+                        periodicConfig = ((periodicConfig << 1) & maxConfig) | ((periodicConfig >> (n - 1)) & 1);
                     }
-                    System.out.println("-------------------------------");
+                } else {
+                    visited[curConfig] = true;
                 }
+                int preimage = ((curConfig & lBoundaryMask) << (n + rr)) | (curConfig << rr) | ((curConfig >> (n - rr)) & rBoundaryMask);
+                int image = 0;
+                for (int j = 0; j < n; j++) {
+                    image <<= 1;
+                    image |= (rule >> ((preimage >> (n - 1 - j)) & ruleMask)) & 1;
+                }
+                curConfig = image;
+                loop++;
             }
             loopCountMap.put(loop, loopCountMap.getOrDefault(loop, 0) + 1);
         }
-
+        System.out.println("rule:\t\t" + r);
+        System.out.println("n:   \t\t" + n);
+        System.out.println("length\tcount");
+        System.out.println("--------------");
+        for (Map.Entry<Integer, Integer> entry : loopCountMap.entrySet()) {
+            System.out.println(entry.getKey() + "\t\t" + entry.getValue());
+        }
     }
 
-    // 打印特定长度的环，移位相等的配置视为不同
-    public static void showCircleWithLength(String r, int lr, int rr, int n, int lengthForPrint, int minNum, int maxNum) {
+    // 打印特定长度的环（规则，左半径，右半径，细胞数，移位相同是否视为同一配置，环长度，起始序号，末尾序号）
+    public static void showCircleWithLength(String r, int lr, int rr, int n, boolean shiftingAsOne, int lengthForPrint, int startNum, int endNum) {
         int d = lr + rr + 1;
         int rule = Tools.getRule(r, 1 << d);
         int ruleMask = (1 << d) - 1;
@@ -176,13 +79,17 @@ public final class injectivityCircleCount {         // d <= 5
             int curConfig = i;
             while (!visited[curConfig]) {
                 evolution.add(curConfig);
-                int periodicConfig = curConfig;
-                for (int k = 0; k < n; k++) {
-                    if (visited[periodicConfig]) {
-                        break;
+                if (shiftingAsOne) {
+                    int periodicConfig = curConfig;
+                    for (int k = 0; k < n; k++) {
+                        if (visited[periodicConfig]) {
+                            break;
+                        }
+                        visited[periodicConfig] = true;
+                        periodicConfig = ((periodicConfig << 1) & maxConfig) | ((periodicConfig >> (n - 1)) & 1);
                     }
-                    visited[periodicConfig] = true;
-                    periodicConfig = ((periodicConfig << 1) & maxConfig) | ((periodicConfig >> (n - 1)) & 1);
+                } else {
+                    visited[curConfig] = true;
                 }
                 int preimage = ((curConfig & lBoundaryMask) << (n + rr)) | (curConfig << rr) | ((curConfig >> (n - rr)) & rBoundaryMask);
                 int image = 0;
@@ -196,7 +103,7 @@ public final class injectivityCircleCount {         // d <= 5
             evolution.add(curConfig);
             if (loop == lengthForPrint) {
                 circleCnt++;
-                if (circleCnt >= minNum && circleCnt <= maxNum) {
+                if (circleCnt >= startNum && circleCnt <= endNum) {
                     System.out.println("Circle No." + circleCnt + ":");
                     for (int idx = 0; idx <= lengthForPrint; idx++) {
                         System.out.println(Tools.toNBitString(evolution.get(idx), n));
